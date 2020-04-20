2eb05498d36c160ea5f8cb205ce911b417a5600f
furkanmtorun <furkanmtorun@gmail.com>
2020-04-20 16:53:58 +0300

    Added colorSchema option for amino acids

--- MSAViewer.js
+++ MSAViewer.js
@@ -1,13 +1,13 @@
 function MSAProcessor({
     fasta,
-    hasConsensus = false
+    hasConsensus = false,
+    colorSchema
 }) {
         this.sequenceDetails = [];
         this.processedSequences = [];
 
         this.extractLinkFromId = function(proteinId){
             var proteinType = proteinId.substring(0, 2);
-            // console.log('hey', prType);
             if (proteinType == "NP" || proteinType == "XP") {
                 link = "https://www.ncbi.nlm.nih.gov/protein/" + proteinId;
             }
@@ -422,23 +422,29 @@ MSAViewer.prototype.loadDivsInViewport = function(reset) {
 
         aaBox.innerHTML = aaLetter;
         aaBox.style.cssText  = 'left:'+(i*20)+'px;';
-        //giving the proper color to each amino acid
-        if (aaLetter.includes("M") === true || aaLetter.includes("C") === true) {aaBox.style.cssText += "background-color:#a5a513"};
-        if (aaLetter.includes("A") === true) {aaBox.style.cssText += "background-color:#C8C8C8"};
-        if (aaLetter.includes("L") === true || aaLetter.includes("V") === true || aaLetter.includes("I") === true) {aaBox.style.cssText += "background-color:#0F820F"};
-        if (aaLetter.includes("D") === true || aaLetter.includes("E") === true) {aaBox.style.cssText += "background-color:#E60A0A"};
-        if (aaLetter.includes("K") === true || aaLetter.includes("R") === true) {aaBox.style.cssText += "background-color:#145AFF"};
-        if (aaLetter.includes("S") === true || aaLetter.includes("T") === true) {aaBox.style.cssText += "background-color:#FA9600"};
-        if (aaLetter.includes("F") === true || aaLetter.includes("Y") === true) {aaBox.style.cssText += "background-color:#3232AA"};
-        if (aaLetter.includes("N") === true || aaLetter.includes("Q") === true) {aaBox.style.cssText += "background-color:#00DCDC"};
-        if (aaLetter.includes("G") === true) {aaBox.style.cssText += "background: #EBEBEB; color: #777;"};
-        if (aaLetter.includes("W") === true) {aaBox.style.cssText += "background-color:#B45AB4"};
-        if (aaLetter.includes("-") === true) {aaBox.style.cssText += "background-color:#333"};
-        if (aaLetter.includes("H") === true) {aaBox.style.cssText += "background-color:#8282D2"};
-        if (aaLetter.includes("P") === true) {aaBox.style.cssText += "background-color:#DC9682"};
-        if (aaBox.style.cssText.indexOf('background') < 0) {
-            aaBox.style.cssText += "background-color:#80a0f0";
+        
+        // Color schema for amino acids
+        colorSchemas = {
+            "hydrophobicity": { A: "#ad0052", B: "#0c00f3", C: "#c2003d", D: "#0c00f3", E: "#0c00f3", F: "#cb0034", G: "#6a0095", H: "#1500ea", I: "#ff0000", J: "#fff", K: "#0000ff", L: "#ea0015", M: "#b0004f", N: "#0c00f3", O: "#fff", P: "#4600b9", Q: "#0c00f3", R: "#0000ff", S: "#5e00a1", T: "#61009e", U: "#fff", V: "#f60009", W: "#5b00a4", X: "#680097", Y: "#4f00b0", Z: "#0c00f3" },
+            "buried": {A: "#00a35c", R: "#00fc03", N: "#00eb14", D: "#00eb14", C: "#0000ff", Q: "#00f10e", E: "#00f10e", G: "#009d62", H: "#00d52a", I: "#0054ab", L: "#007b84", K: "#00ff00", M: "#009768", F: "#008778", P: "#00e01f", S: "#00d52a", T: "#00db24", W: "#00a857", Y: "#00e619", V: "#005fa0", B: "#00eb14", X: "#00b649", Z: "#00f10e"},
+            "cinema": {A: "#BBBBBB", B: "grey", C: "yellow", D: "red", E: "red", F: "magenta", G: "brown", H: "#00FFFF", I: "#BBBBBB", J: "#fff", K: "#00FFFF", L: "#BBBBBB", M: "#BBBBBB", N: "green", O: "#fff", P: "brown", Q: "green", R: "#00FFFF", S: "green", T: "green", U: "#fff", V: "#BBBBBB", W: "magenta", X: "grey", Y: "magenta", Z: "grey"},
+            "clustal": {A: "orange", B: "#fff", C: "green", D: "red", E: "red", F: "blue", G: "orange", H: "red", I: "green", J: "#fff", K: "red", L: "green", M: "green", N: "#fff", O: "#fff", P: "orange", Q: "#fff", R: "red", S: "orange", T: "orange", U: "#fff", V: "green", W: "blue", X: "#fff", Y: "blue", Z: "#fff"},
+            "clustal2": {A: "#80a0f0", R: "#f01505", N: "#00ff00", D: "#c048c0", C: "#f08080", Q: "#00ff00", E: "#c048c0", G: "#f09048", H: "#15a4a4", I: "#80a0f0", L: "#80a0f0", K: "#f01505", M: "#80a0f0", F: "#80a0f0", P: "#ffff00", S: "#00ff00", T: "#00ff00", W: "#80a0f0", Y: "#15a4a4", V: "#80a0f0", B: "#fff", X: "#fff", Z: "#fff"},
+            "helix": {A: "#e718e7", R: "#6f906f", N: "#1be41b", D: "#778877", C: "#23dc23", Q: "#926d92", E: "#ff00ff", G: "#00ff00", H: "#758a75", I: "#8a758a", L: "#ae51ae", K: "#a05fa0", M: "#ef10ef", F: "#986798", P: "#00ff00", S: "#36c936", T: "#47b847", W: "#8a758a", Y: "#21de21", V: "#857a85", B: "#49b649", X: "#758a75", Z: "#c936c9"},
+            "lesk": {A: " orange", B: " #fff", C: " green", D: " red", E: " red", F: " green", G: " orange", H: " magenta", I: " green", J: " #fff", K: " red", L: " green", M: " green", N: " magenta", O: " #fff", P: " green", Q: " magenta", R: " red", S: " orange", T: " orange", U: " #fff", V: " green", W: " green", X: " #fff", Y: " green", Z: " #fff"},
+            "mae": {A: " #77dd88", B: " #fff", C: " #99ee66", D: " #55bb33", E: " #55bb33", F: " #9999ff", G: " #77dd88", H: " #5555ff", I: " #66bbff", J: " #fff", K: " #ffcc77", L: " #66bbff", M: " #66bbff", N: " #55bb33", O: " #fff", P: " #eeaaaa", Q: " #55bb33", R: " #ffcc77", S: " #ff4455", T: " #ff4455", U: " #fff", V: " #66bbff", W: " #9999ff", X: " #fff", Y: " #9999ff", Z: " #fff"},
+            "strand": {A: "#5858a7", R: "#6b6b94", N: "#64649b", D: "#2121de", C: "#9d9d62", Q: "#8c8c73", E: "#0000ff", G: "#4949b6", H: "#60609f", I: "#ecec13", L: "#b2b24d", K: "#4747b8", M: "#82827d", F: "#c2c23d", P: "#2323dc", S: "#4949b6", T: "#9d9d62", W: "#c0c03f", Y: "#d3d32c", V: "#ffff00", B: "#4343bc", X: "#797986", Z: "#4747b8"},
+            "taylor": {A: "#ccff00", R: "#0000ff", N: "#cc00ff", D: "#ff0000", C: "#ffff00", Q: "#ff00cc", E: "#ff0066", G: "#ff9900", H: "#0066ff", I: "#66ff00", L: "#33ff00", K: "#6600ff", M: "#00ff00", F: "#00ff66", P: "#ffcc00", S: "#ff3300", T: "#ff6600", W: "#00ccff", Y: "#00ffcc", V: "#99ff00", B: "#fff", X: "#fff", Z: "#fff"},
+            "turn": {A: "#2cd3d3", R: "#708f8f", N: "#ff0000", D: "#e81717", C: "#a85757", Q: "#3fc0c0", E: "#778888", G: "#ff0000", H: "#708f8f", I: "#00ffff", L: "#1ce3e3", K: "#7e8181", M: "#1ee1e1", F: "#1ee1e1", P: "#f60909", S: "#e11e1e", T: "#738c8c", W: "#738c8c", Y: "#9d6262", V: "#07f8f8", B: "#f30c0c", X: "#7c8383", Z: "#5ba4a4"},
+            "zappo": {A: "#ffafaf", R: "#6464ff", N: "#00ff00", D: "#ff0000", C: "#ffff00", Q: "#00ff00", E: "#ff0000", G: "#ff00ff", H: "#6464ff", I: "#ffafaf", L: "#ffafaf", K: "#6464ff", M: "#ffafaf", F: "#ffc800", P: "#ff00ff", S: "#00ff00", T: "#00ff00", W: "#ffc800", Y: "#ffc800", V: "#ffafaf", B: "#fff", X: "#fff", Z: "#fff"},
         }
+        var aaColor = colorSchemas[colorSchema][aaLetter]
+        aaBox.style.backgroundColor += aaColor;
+        // Special cases for color schema
+        if ((aaColor == "#fff") || (aaColor == "yellow")) {aaBox.style.color += "#555"};
+        // Consensus
+        if ((aaLetter == ".") || (aaLetter == ":")) {aaBox.style.backgroundColor += "#5c5c5c"};
+
         documentFragment.appendChild(aaBox);
         aaBox = null;
     }
