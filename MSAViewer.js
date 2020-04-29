433b157d8a8cf3162e52a5c87704b15f3b5aa266
Halil Bilgin <bilginhalil@gmail.com>
2020-04-29 18:18:31 +0300

    Merge changes from master

--- MSAViewer.js
+++ MSAViewer.js
@@ -1,15 +1,13 @@
-var inject = (str, obj) => str.replace(/\${(.*?)}/g, (x,g)=> obj[g]);
-
 function MSAProcessor({
     fasta,
-    hasConsensus = false
+    hasConsensus = false,
+    colorSchema
 }) {
         this.sequenceDetails = [];
         this.processedSequences = [];
 
         this.extractLinkFromId = function(proteinId){
             var proteinType = proteinId.substring(0, 2);
-            // console.log('hey', prType);
             if (proteinType == "NP" || proteinType == "XP") {
                 link = "https://www.ncbi.nlm.nih.gov/protein/" + proteinId;
             }
@@ -179,16 +177,17 @@ function MSAViewer({   // notice the curly braces! we are receiving an object no
             protein.className = "protein";
             var speciesName = document.createElement("div");
             var speciesNameLink = document.createElement("a");
+            var speciesTooltip = document.createElement('span');
             
             speciesNameLink.setAttribute("href", sequenceDetails.link);
             speciesNameLink.setAttribute('target', '_blank');
-            speciesNameLink.setAttribute('class', 'tooltipped');
-            speciesNameLink.setAttribute('data-position', 'right');
-            speciesNameLink.setAttribute('data-tooltip', sequenceDetails.proteinId);
+            speciesTooltip.setAttribute('class', 'tooltiptext');
+            speciesTooltip.innerHTML = sequenceDetails.proteinId;
 
             document.getElementById(ids.speciesNames).appendChild(speciesName).appendChild(speciesNameLink);
-            speciesName.className = "species-name";
-            speciesNameLink.appendChild(document.createTextNode(sequenceDetails.species)); //// exception case olacak 
+            document.getElementById(ids.speciesNames).appendChild(speciesName).appendChild(speciesTooltip);
+            speciesName.className = "species-name tooltip";
+            speciesNameLink.appendChild(document.createTextNode(sequenceDetails.species));
         }
 
     }
@@ -229,6 +228,7 @@ function MSAViewer({   // notice the curly braces! we are receiving an object no
     }
 
     this.showVariation = function(prNumber, aaNumber) {
+
         let textBox = document.createElement("div");
         let innerTextBox = document.createElement("div");
         textBox.setAttribute("class", "variation-text-box");
@@ -330,7 +330,7 @@ MSAViewer.prototype.loadAminoacidSearch = function() {
         Species : <select name="species" id="${ids.speciesSelect}"></select><br>
         </section>`;
         
-    highlightColumnTemplate = '<div class="highlight-column position-number" style="left:${alignmentPosition}px">${position}</div>';
+
     if($mainDiv.find('.go-to-position').length == 0)
         $mainDiv.append(containerTemplate);
     
@@ -342,10 +342,9 @@ MSAViewer.prototype.loadAminoacidSearch = function() {
         $mainDiv.find('.highlight-column').removeClass('highlight-column ptm-highlighted');
         $mainDiv.find('.position-number').remove();
         
-        $mainDiv.find('.protein:eq(0)').append(inject(highlightColumnTemplate, {
-            alignmentPosition: alignmentPosition * 20,
-            position: position
-        }));
+        template = `<div class="highlight-column position-number" style="left:${alignmentPosition * 20}px">${position}</div>`;
+
+        $mainDiv.find('.protein:eq(0)').append(template);
 
         $mainDiv.scrollLeft(alignmentPosition * 20 - ($mainDiv.width() - 160) / 2)
 
@@ -357,7 +356,8 @@ MSAViewer.prototype.loadAminoacidSearch = function() {
 
     for(var i in this.msa.sequenceDetails) {
         var species = this.msa.sequenceDetails[i].species;
-        $('#'+ids.speciesSelect).html($('#'+ids.speciesSelect).html() + '<option value="' + i + '">' + species + '</option>');
+        var template = `<option value="${i}">${species}</option>`;
+        $('#'+ids.speciesSelect).append(template);
     }
 
 
@@ -428,23 +428,30 @@ MSAViewer.prototype.loadDivsInViewport = function(reset) {
 
         aaBox.innerHTML = aaLetter;
         aaBox.style.cssText  = 'left:'+(i*20)+'px;';
-        //giving the proper color to each amino acid
-        if (aaLetter.includes("M") === true || aaLetter.includes("C") === true) {aaBox.style.cssText += "background-color:#a5a513"};
-        if (aaLetter.includes("A") === true) {aaBox.style.cssText += "background-color:#C8C8C8"};
-        if (aaLetter.includes("L") === true || aaLetter.includes("V") === true || aaLetter.includes("I") === true) {aaBox.style.cssText += "background-color:#0F820F"};
-        if (aaLetter.includes("D") === true || aaLetter.includes("E") === true) {aaBox.style.cssText += "background-color:#E60A0A"};
-        if (aaLetter.includes("K") === true || aaLetter.includes("R") === true) {aaBox.style.cssText += "background-color:#145AFF"};
-        if (aaLetter.includes("S") === true || aaLetter.includes("T") === true) {aaBox.style.cssText += "background-color:#FA9600"};
-        if (aaLetter.includes("F") === true || aaLetter.includes("Y") === true) {aaBox.style.cssText += "background-color:#3232AA"};
-        if (aaLetter.includes("N") === true || aaLetter.includes("Q") === true) {aaBox.style.cssText += "background-color:#00DCDC"};
-        if (aaLetter.includes("G") === true) {aaBox.style.cssText += "background: #EBEBEB; color: #777;"};
-        if (aaLetter.includes("W") === true) {aaBox.style.cssText += "background-color:#B45AB4"};
-        if (aaLetter.includes("-") === true) {aaBox.style.cssText += "background-color:#333"};
-        if (aaLetter.includes("H") === true) {aaBox.style.cssText += "background-color:#8282D2"};
-        if (aaLetter.includes("P") === true) {aaBox.style.cssText += "background-color:#DC9682"};
-        if (aaBox.style.cssText.indexOf('background') < 0) {
-            aaBox.style.cssText += "background-color:#80a0f0";
+        
+        // Color schema for amino acids
+        colorSchemas = {
+            "hydrophobicity": { A: "#ad0052", B: "#0c00f3", C: "#c2003d", D: "#0c00f3", E: "#0c00f3", F: "#cb0034", G: "#6a0095", H: "#1500ea", I: "#ff0000", J: "#fff", K: "#0000ff", L: "#ea0015", M: "#b0004f", N: "#0c00f3", O: "#fff", P: "#4600b9", Q: "#0c00f3", R: "#0000ff", S: "#5e00a1", T: "#61009e", U: "#fff", V: "#f60009", W: "#5b00a4", X: "#680097", Y: "#4f00b0", Z: "#0c00f3" },
+            "buried": {A: "#00a35c", R: "#00fc03", N: "#00eb14", D: "#00eb14", C: "#0000ff", Q: "#00f10e", E: "#00f10e", G: "#009d62", H: "#00d52a", I: "#0054ab", L: "#007b84", K: "#00ff00", M: "#009768", F: "#008778", P: "#00e01f", S: "#00d52a", T: "#00db24", W: "#00a857", Y: "#00e619", V: "#005fa0", B: "#00eb14", X: "#00b649", Z: "#00f10e"},
+            "cinema": {A: "#BBBBBB", B: "grey", C: "yellow", D: "red", E: "red", F: "magenta", G: "brown", H: "#00FFFF", I: "#BBBBBB", J: "#fff", K: "#00FFFF", L: "#BBBBBB", M: "#BBBBBB", N: "green", O: "#fff", P: "brown", Q: "green", R: "#00FFFF", S: "green", T: "green", U: "#fff", V: "#BBBBBB", W: "magenta", X: "grey", Y: "magenta", Z: "grey"},
+            "clustal": {A: "orange", B: "#fff", C: "green", D: "red", E: "red", F: "blue", G: "orange", H: "red", I: "green", J: "#fff", K: "red", L: "green", M: "green", N: "#fff", O: "#fff", P: "orange", Q: "#fff", R: "red", S: "orange", T: "orange", U: "#fff", V: "green", W: "blue", X: "#fff", Y: "blue", Z: "#fff"},
+            "clustal2": {A: "#80a0f0", R: "#f01505", N: "#00ff00", D: "#c048c0", C: "#f08080", Q: "#00ff00", E: "#c048c0", G: "#f09048", H: "#15a4a4", I: "#80a0f0", L: "#80a0f0", K: "#f01505", M: "#80a0f0", F: "#80a0f0", P: "#ffff00", S: "#00ff00", T: "#00ff00", W: "#80a0f0", Y: "#15a4a4", V: "#80a0f0", B: "#fff", X: "#fff", Z: "#fff"},
+            "helix": {A: "#e718e7", R: "#6f906f", N: "#1be41b", D: "#778877", C: "#23dc23", Q: "#926d92", E: "#ff00ff", G: "#00ff00", H: "#758a75", I: "#8a758a", L: "#ae51ae", K: "#a05fa0", M: "#ef10ef", F: "#986798", P: "#00ff00", S: "#36c936", T: "#47b847", W: "#8a758a", Y: "#21de21", V: "#857a85", B: "#49b649", X: "#758a75", Z: "#c936c9"},
+            "lesk": {A: " orange", B: " #fff", C: " green", D: " red", E: " red", F: " green", G: " orange", H: " magenta", I: " green", J: " #fff", K: " red", L: " green", M: " green", N: " magenta", O: " #fff", P: " green", Q: " magenta", R: " red", S: " orange", T: " orange", U: " #fff", V: " green", W: " green", X: " #fff", Y: " green", Z: " #fff"},
+            "mae": {A: " #77dd88", B: " #fff", C: " #99ee66", D: " #55bb33", E: " #55bb33", F: " #9999ff", G: " #77dd88", H: " #5555ff", I: " #66bbff", J: " #fff", K: " #ffcc77", L: " #66bbff", M: " #66bbff", N: " #55bb33", O: " #fff", P: " #eeaaaa", Q: " #55bb33", R: " #ffcc77", S: " #ff4455", T: " #ff4455", U: " #fff", V: " #66bbff", W: " #9999ff", X: " #fff", Y: " #9999ff", Z: " #fff"},
+            "strand": {A: "#5858a7", R: "#6b6b94", N: "#64649b", D: "#2121de", C: "#9d9d62", Q: "#8c8c73", E: "#0000ff", G: "#4949b6", H: "#60609f", I: "#ecec13", L: "#b2b24d", K: "#4747b8", M: "#82827d", F: "#c2c23d", P: "#2323dc", S: "#4949b6", T: "#9d9d62", W: "#c0c03f", Y: "#d3d32c", V: "#ffff00", B: "#4343bc", X: "#797986", Z: "#4747b8"},
+            "taylor": {A: "#ccff00", R: "#0000ff", N: "#cc00ff", D: "#ff0000", C: "#ffff00", Q: "#ff00cc", E: "#ff0066", G: "#ff9900", H: "#0066ff", I: "#66ff00", L: "#33ff00", K: "#6600ff", M: "#00ff00", F: "#00ff66", P: "#ffcc00", S: "#ff3300", T: "#ff6600", W: "#00ccff", Y: "#00ffcc", V: "#99ff00", B: "#fff", X: "#fff", Z: "#fff"},
+            "turn": {A: "#2cd3d3", R: "#708f8f", N: "#ff0000", D: "#e81717", C: "#a85757", Q: "#3fc0c0", E: "#778888", G: "#ff0000", H: "#708f8f", I: "#00ffff", L: "#1ce3e3", K: "#7e8181", M: "#1ee1e1", F: "#1ee1e1", P: "#f60909", S: "#e11e1e", T: "#738c8c", W: "#738c8c", Y: "#9d6262", V: "#07f8f8", B: "#f30c0c", X: "#7c8383", Z: "#5ba4a4"},
+            "zappo": {A: "#ffafaf", R: "#6464ff", N: "#00ff00", D: "#ff0000", C: "#ffff00", Q: "#00ff00", E: "#ff0000", G: "#ff00ff", H: "#6464ff", I: "#ffafaf", L: "#ffafaf", K: "#6464ff", M: "#ffafaf", F: "#ffc800", P: "#ff00ff", S: "#00ff00", T: "#00ff00", W: "#ffc800", Y: "#ffc800", V: "#ffafaf", B: "#fff", X: "#fff", Z: "#fff"},
+            "nucleotide": { A: " #64F73F", C: " #FFB340", G: " #EB413C", T: " #3C88EE", U: " #3C88EE" }
         }
+        var aaColor = colorSchemas[colorSchema][aaLetter]
+        aaBox.style.backgroundColor += aaColor;
+        // Special cases for color schema
+        if ((aaColor == "#fff") || (aaColor == "yellow")) {aaBox.style.color += "#555"};
+        // Consensus
+        if ((aaLetter == ".") || (aaLetter == ":")) {aaBox.style.backgroundColor += "#5c5c5c"};
+
         documentFragment.appendChild(aaBox);
         aaBox = null;
     }
@@ -489,6 +496,7 @@ MSAViewer.prototype.addDomains = function(domains) {
 }
 
 MSAViewer.prototype.addVariation = function(protein, aminoacid, variationNote, source) {
+    var protein = protein - 1; // the species start from 0
     let aaNumber = aminoacid - 1; // the aacids start from 0
 
     notesByProtein = this.variationNotes[protein];
---
+++ README.md
@@ -0,0 +1,157 @@
+# 🧬 MSAViewer
+
+**Contents**
+
+- [🧬 MSAViewer](#%f0%9f%a7%ac-msaviewer)
+	- [:hash: What is *MSAViewer* and the aim of this library?](#hash-what-is-msaviewer-and-the-aim-of-this-library)
+	- [:hash: Requirements and Installation](#hash-requirements-and-installation)
+	- [:hash: How to use *MSAViewer*?](#hash-how-to-use-msaviewer)
+	- [:hash: Parameters & Examples for the Functions](#hash-parameters--examples-for-the-functions)
+		- [:hash: Color Schemas List](#hash-color-schemas-list)
+		- [:hash: Adding Protein Domains & Example](#hash-adding-protein-domains--example)
+		- [:hash: ``addVariation()`` Function & Example Use](#hash-addvariation-function--example-use)
+	- [:hash: Example Usages (Use Cases) of MSAViewer](#hash-example-usages-use-cases-of-msaviewer)
+		- [:hash: Evolutionary Genomics Study](#hash-evolutionary-genomics-study)
+		- [:hash: Clinical Variant Interpretation Study](#hash-clinical-variant-interpretation-study)
+		- [:hash: COVID-19 / Virology Study](#hash-covid-19--virology-study)
+		- [:hash: Do you have another study which is not listed here?](#hash-do-you-have-another-study-which-is-not-listed-here)
+	- [:hash: Contributing & Feedback](#hash-contributing--feedback)
+	- [:hash: Developers](#hash-developers)
+
+---
+
+## :hash: What is *MSAViewer* and the aim of this library?
+*MSAViewer* is an open-source, flexible and modern web-based tool to illustrate the genetic variants, post-translational modifications, and protein domainson the corresponding positions of aminoacids at the protein sequence of any species together with pairwise or multiple sequence alignment of anysize. Ever-increasing number of genetic variants and sequencing projects in next-generation sequencingera enable researchers to investigate the impacts of these variants together with the protein domains. Here, MSAViewer environment and its core features make it easy to visualize the evolutionary conserved amino acids and genetic variants. 
+
+
+## :hash: Requirements and Installation
+*MSAViewer* is entirely developed in JavaScript and works on a web browser at any platform including Linux, Mac OS X and Windows systems without any installation.
+
+## :hash: How to use *MSAViewer*?
+ - Create a directory and download any example shown below or retrieve "**MSAViewer.js**" and "**index.html**" files or clone the repository via Git using following command:
+ 
+ 	`git clone https://github.com/thekaplanlab/msaviewer.git`
+
+- Then, place your pairwise or multiple sequence alignment (MSA) result file as **FASTA** format in the folder.
+
+- Afterwards, set your parameters and define the title, give protein domains information and add your variants onto any species. 
+
+- It's ready to use and visualize now! 
+
+## :hash: Parameters & Examples for the Functions
+| Options in the script | Description | Example |
+|--|--|--|
+| id | *It defines the ID of the element where you place MSAViewer component.* |`<section id="MSAViewerDemo"></section>` |
+| fasta | *It refers a variable that holds your sequence alignment or the name of the file in FASTA format* | `sample_msa.txt` or `sample_msa.fasta`
+| title | *It defines the title of MSAViewer component on the box at the top left corner* | HARS Protein
+| hasConsensus  | It asks whether you would like to display the consensus sequence or not. | Please state as either `true` or `false`.
+| colorSchema  | It defines the name of the color schema you would like to display | Please the check the list of color schemas below.
+| domains  | It refers a variable that holds your protein domanin information. | Please check the example below.
+| ``addVariation()`` function  | It serves for adding variations on the corresponding amino acids. | Please check the example below.
+
+### :hash: Color Schemas List
+
+> You need to select a color schema and put the name of the color schema into the variable:
+
+Example for this:
+> `var colorSchema = "clustal";`
+
+**List of the color schemas:**
+
+- `hydrophobicity`
+- `buried`
+- `cinema`
+- `clustal`
+- `clustal2`
+- `helix`
+- `lesk`
+- `mae`
+- `strand`
+- `taylor`
+- `turn`
+- `zappo`
+- `nucleotide`
+
+
+### :hash: Adding Protein Domains & Example
+
+> You are able to visualize the protein domains above the sequence aligner.
+
+Here is the example use of `domains` variable.
+```
+var domains = [
+	{
+		'domain_id': "PFAM123",
+		'domain_name': "Protein Kinase Domain",
+		'domain_external_link': "http://link_for_the_domain.com/",
+		'domain_start_point': 11,
+		'domain_end_point': 32
+	},
+	{
+		'domain_id': "PFAM456",
+		'domain_name': "Another Protein Domain",
+		'domain_external_link': "http://link_for_the_domain2.com/",
+		'domain_start_point': 35,
+		'domain_end_point': 55
+	}
+];
+```
+
+### :hash: ``addVariation()`` Function & Example Use
+
+> You are able to add variations and substitutions on the corresponding amino acids.
+
+**Here is the details of ``addVariation()`` function:**
+
+
+| Parameter | Description | Example |
+|---------------------------------------------------|--------------------------------------------------------|----------------------------------------------------------|
+| protein | The order of the species in the sequence alignment | 1 |
+| aminoacid | The (real) position of the amino acid in the protein.* | 5 |
+| variationNote | The annotation part of this variation. | M->A : Pathogenic and causes a disease with a name of X. |
+| source | The source of the information. | Surname et. al (2020) |
+
+
+> For instance, an amino acid in a protein in the position of 15 has a substitution. However, since the sequences are aligned and some dashes `-` added into the sequence, you do not need to consider the new position of the amino acid. Only, put the real position of the amino acid. 
+
+**Here is the example how to add a variation:**
+```
+viewer.addVariation(1, 5, "M->A : Pathogenic and causes a disease with a name of X", "Surname et. al (2020)");
+```
+
+> Here, this variation will be added onto the `5th` amino acid in the protein of `1st` species in the sequence alignment with a note of "`M->A : Pathogenic and causes a disease with a name of X`" and source of "`Surname et. al (2020)`". 
+
+
+> In addition, if you state the `source` as `"PTM"` (post-translational modification), it also will be notified as red asterisk in the viewer.
+
+
+## :hash: Example Usages (Use Cases) of MSAViewer
+
+### :hash: Evolutionary Genomics Study 
+Will be added.
+
+### :hash: Clinical Variant Interpretation Study 
+Will be added.
+
+### :hash: COVID-19 / Virology Study 
+Will be added.
+
+### :hash: Do you have another study which is not listed here?
+Please do not hesitate to contact us for your study. 
+
+We would be very happy to list your study here.
+
+
+## :hash: Contributing & Feedback
+MSAViewer is released as an open-source and web-based software under GNU General Public License, version 3.0 (GPLv3). The visualizer, documentation, all source code and examples are available at http://MSAViewer.github.io/ and GitHub repository https://github.com/thekaplanlab/MSAViewer.
+
+Moreover, please do not hesitate to open an issue via Github if you have any suggestion or feedback.
+
+
+## :hash: Developers
+**Halil Bilgin |  [bilginhalil@gmail.com](mailto:bilginhalil@gmail.com) | Academia: [Google Scholar Profile](https://scholar.google.com/citations?user=U1jyUGkAAAAJ&hl=en&oi=ao)**
+
+
+**Furkan Torun |  [furkanmtorun@gmail.com](mailto:furkanmtorun@gmail.com) | Academia: [Google Scholar Profile](https://scholar.google.com/citations?user=d5ZyOZ4AAAAJ)**
+
+
