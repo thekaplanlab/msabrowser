009988c10cc5aba46a0b1f9a30e5632fb3a0da1a
Halil Bilgin <bilginhalil@gmail.com>
2020-05-09 11:42:12 +0100

    Display a loading spinner whilst preparing the PNG

--- MSAViewer.js
+++ MSAViewer.js
@@ -582,10 +582,14 @@ MSAViewer.prototype.export = function (fileName) {
     var hrefTag = "data:text/plain;charset=UTF-8," + encodeURIComponent(fileContent);
     this.mainDiv.find('.bottom-panel').append('<a class="msa-button export-button" href="' + hrefTag + '" download="' + fileName + '">Download as FASTA</a>');
     this.mainDiv.find('.bottom-panel').append('<a href="javascript:void(0)" class="msa-button ss-button">Save as PNG</a>');
+
     this.mainDiv.find('.ss-button').click(() => {
+        this.mainDiv.find('.ss-button').prepend('<img src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/fancybox_loading.gif" style="height:18px"> ')
+
         html2canvas(this.mainDiv.find('.scroll-container')[0], { height: this.mainDiv.height(), width: this.mainDiv.width() }).then(canvas => {
             console.log(canvas);
             saveAs(canvas.toDataURL(), 'msa-browser-image.png');
+            this.mainDiv.find('.ss-button').find('img').remove();
         });
     })
 }
